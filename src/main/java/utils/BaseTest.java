6905b5c0c6ac4a3902f76e89fc355149477cfc4a
unknown <andrei.mihai3110@gmail.com>
2023-10-05 09:29:13 +0300

    initial commit

---
+++ src/main/java/utils/BaseTest.java
@@ -0,0 +1,33 @@
+package utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import pages.BasePage;
+
+public class BaseTest {
+	
+	public static WebDriver driver;
+	public BasePage app;
+	
+	@BeforeClass
+	public void setup() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+		driver.get("https://keybooks.ro/");
+		//driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		app = new BasePage();
+	}
+
+	@AfterClass
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(4000); //bad practice
+		//driver.close();
+		driver.quit();
+	}
+}
