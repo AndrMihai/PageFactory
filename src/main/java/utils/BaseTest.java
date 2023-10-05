046340645d561412666514a025489426aea50824
unknown <andrei.mihai3110@gmail.com>
2023-10-05 11:30:16 +0300

    curs 34

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -1,10 +1,19 @@
 package utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
+
+import com.google.common.io.Files;
 
 import pages.BasePage;
 
@@ -12,22 +21,38 @@ public class BaseTest {
 	
 	public static WebDriver driver;
 	public BasePage app;
-	
-	@BeforeClass
-	public void setup() {
+	@Parameters({"appURL"})
+	@BeforeClass(alwaysRun = true)
+	public void setup(String orice) {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		//driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
-		driver.get("https://keybooks.ro/");
+		driver.get(orice); //sau cu URL keyfood.ro, aici parsam parametrul orice
 		//driver.get("https://the-internet.herokuapp.com/javascript_alerts");
-		app = new BasePage();
+		app = new BasePage(driver);
 	}
 
-	@AfterClass
+	@AfterClass(alwaysRun = true)
 	public void tearDown() throws InterruptedException {
 		Thread.sleep(4000); //bad practice
 		//driver.close();
 		driver.quit();
+	}
+	
+	@AfterMethod
+	public void recordFailure(ITestResult result) {
+		
+		if(ITestResult.FAILURE == result.getStatus()) {
+			TakesScreenshot poza = (TakesScreenshot) driver;
+			File picture = poza.getScreenshotAs(OutputType.FILE);
+			String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+			try {
+				Files.copy(picture, new File("poze/"+result.getName()+" - "+timestamp+ ".png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 }
